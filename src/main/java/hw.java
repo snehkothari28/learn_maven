dbd53356fada71e8791d30fe73bf9a9cc1b29078
snehk <sneh.kothari28@gmail.com>
2021-12-05 17:00:40 +0530

    .

---
+++ src/main/java/hw.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class hw {
+    public static void main(String[] args) {
+        System.out.println("sskksks");
+        System.out.println(StringUtils.capitalize("ss"));
+    }
+}
